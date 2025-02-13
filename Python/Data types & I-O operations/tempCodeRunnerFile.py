63a52f349a675a2536c787175aacc0b41b565504
Manjunath_05 <saipathapadu@gmail.com>
2025-02-13 19:52:32 +0530

    Python

---
+++ Python/Data types & I-O operations/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+x=input("Enter the value of x :")
+y=input("Enter the value of y :")
+print("sum of a , b = ",x+y) # Here  it adds combines the number bcoz input function accepts the string values only we have to type-caste them
+print(type(x))
